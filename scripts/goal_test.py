d8f0177031ab6ba60a25533bf3627b36aba3d8e1
AmrMohamed <amr.moh@hotmail.com>
2019-03-12 12:23:50 +0200

    First Commit

---
+++ scripts/goal_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int32MultiArray
+
+rospy.init_node('tester2')
+pub = rospy.Publisher('/goal1', Int32MultiArray, queue_size=10)
+
+array = Int32MultiArray()
+array.data= [8,0,0]
+
+if __name__ == '__main__':
+	while not rospy.is_shutdown():
+		pub.publish(array)
---
+++ scripts/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int32MultiArray
+
+rospy.init_node('tester')
+pub = rospy.Publisher('/rob1_CurrentPose', Int32MultiArray, queue_size=10)
+
+array = Int32MultiArray()
+array.data= [0,0,0]
+
+if __name__ == '__main__':
+	while not rospy.is_shutdown():
+		pub.publish(array)
